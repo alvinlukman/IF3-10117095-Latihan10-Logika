9b2ef93d8ed2fb2ab01ea3d423c70a0f386c540d
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:46:48 +0700

    Logika

---
+++ src/if3/pkg10117095/latihan10/logika/IF310117095Latihan10Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berisi contoh operator Logika dengan variabel
+ */
+public class IF310117095Latihan10Logika {
+    
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
